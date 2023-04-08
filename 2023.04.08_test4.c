45b30a9301e989f28e61ef6c9a3d0e572f0ac8f7
cc031023 <1041526008@qq.com>
2023-04-08 20:44:34 +0800

    作业4

---
+++ 2023.04.08_test4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    while (~scanf("%d%d%d", &a, &b, &c)) // 使得a > b > c.
+    {
+        if (a < b)
+        {
+            a += b;
+            b = a - b;
+            a -= b;
+        }
+        if (a < c)
+        {
+            a += c;
+            c = a - c;
+            a -= c;
+        }
+        if (b < c)
+        {
+            b += c;
+            c = b - c;
+            b -= c;
+        }
+        if (b + c <= a)
+        {
+            printf("Not a triangle!\n");
+        }
+        else
+        {
+            if (a == b && b == c)
+            {
+                printf("Equilateral triangle!\n");
+            }
+            else if (a == b || b == c)
+            {
+                printf("Isosceles triangle!\n");
+            }
+            else
+            {
+                printf("Ordinary triangle!\n");
+            }
+        }
+    }
+    return 0;
+}
