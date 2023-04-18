e955ebb31aab46737e704e3b0cab97b41ba3604d
cc031023 <1041526008@qq.com>
2023-04-18 18:08:23 +0800

    1到100打印3的倍数.

--- 2023.04.16_test1.c
+++ 2023.04.16_test1.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
+// 从1到100打印3的倍数.
 int main()
 {
     int i = 1;
-    for (i = 1; i <= 100; ++i) // 从1到100打印3的倍数.
-    {
+    for (i = 1; i <= 100; ++i)
         if (i % 3 == 0)
             printf("%d ", i);
-    }
     return 0;
 }
