73e35c577bfe32dcd1b2e3a66f2dca8d8b5e59bb
cc031023 <1041526008@qq.com>
2023-04-18 18:07:51 +0800

    简单算术.

--- 2023.04.08_test6.c
+++ 2023.04.08_test6.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 const int N = 3.156e7;
+// 简单的算术题注意age可能溢出.
 int main()
 {
-    long long age;
+    long long age = 0;
     scanf("%lld", &age);
     printf("%lld", age * N);
     return 0;
