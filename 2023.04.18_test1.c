27c9abc0946357fcfb3898816ffacbb14f3cc52d
cc031023 <1041526008@qq.com>
2023-04-18 18:14:40 +0800

    打印9 * 9乘法表.

--- 2023.04.18_test1.c
+++ 2023.04.18_test1.c
@@ -5,6 +5,6 @@ int main()
     int i = 0, j = 0;
     for (i = 1; i <= 9; ++i)
         for (j = 1; j <= i; ++j)
-            printf("%d * %d = %-2d%c", j, i, i * j, " \n"[j == i]);
+            printf("%d * %d = %-2d %c", j, i, i * j, " \n"[j == i]);
     return 0;
 }
