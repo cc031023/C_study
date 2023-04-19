a26a5a640685e483e21bc3ae98b0eeb46d30a1d0
cc031023 <1041526008@qq.com>
2023-04-20 00:09:59 +0800

    // 简单购物应用题.

---
+++ 2023.04.20_test5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// 简单购物应用题.
+int main()
+{
+    double val = 0;
+    int month = 0, day = 0, num = 0;
+    scanf("%lf%d%d%d", &val, &month, &day, &num);
+    if (month == day)
+    {
+        if (month == 11)
+        {
+            val *= 0.7;
+            val -= num * 50;
+        }
+        else if (month == 12)
+        {
+            val *= 0.8;
+            val -= num * 50;
+        }
+    }
+    printf("%.2lf", val > 0 ? val : 0);
+    return 0;
+}
