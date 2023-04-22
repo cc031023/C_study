b677df5a99860a41856b59b7f9942ef7c9001bee
cc031023 <1041526008@qq.com>
2023-04-22 19:16:31 +0800

    用自定义函数打印n * n乘法表.

--- 2023.04.22_test1.c
+++ 2023.04.22_test1.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 // 打印n * n乘法表.
-int main()
+
+inline void myprint(int n)
 {
-    int i = 0, j = 0, n = 0;
-    scanf("%d", &n);
+    int i = 0, j = 0;
     for (i = 1; i <= n; ++i)
         for (j = 1; j <= i; ++j)
             printf("%d * %d = %-2d%c", j, i, i * j, " \n"[j == i]);
+}
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    myprint(n);
     return 0;
 }
