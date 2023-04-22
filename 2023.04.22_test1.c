13e2b32c853f834efb2b7d0686c601485bca0854
cc031023 <1041526008@qq.com>
2023-04-22 19:10:10 +0800

    打印n * n乘法表.

--- 2023.04.22_test1.c
+++ 2023.04.22_test1.c
@@ -2,7 +2,7 @@
 // 打印n * n乘法表.
 int main()
 {
-    int i = 0, j = 0, n = 0, m = 0;
+    int i = 0, j = 0, n = 0;
     scanf("%d", &n);
     for (i = 1; i <= n; ++i)
         for (j = 1; j <= i; ++j)
