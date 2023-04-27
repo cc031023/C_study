313fcaecf2675179547618c89440e157115efae5
cc031023 <1041526008@qq.com>
2023-04-27 19:46:27 +0800

    自定义函数实现反转字符串.

--- 2023.04.27_test4.c
+++ 2023.04.27_test4.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+
+inline int myStrlen(char *arr) // 递归.
+{
+    if (*arr == '\0')
+        return 0;
+    return myStrlen(arr + 1) + 1;
+}
 
 inline void reverse_string(char *string)
 {
-    if (strlen(string) < 2)
+    if (myStrlen(string) < 2)
         return;
     char temp = *string;
-    int len = strlen(string);
+    int len = myStrlen(string);
     *string = *(string + len - 1);
     *(string + len - 1) = '\0';
     reverse_string(string + 1);
